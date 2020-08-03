0e36f66b54cc38d5781cde274e8ca81d4521f81a
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-08-03 16:23:59 -0500

    Add color palette to tailwind

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useButton } from '@react-aria/button'
 
 export function Button({ ...props }) {
-  return <button {...props} />
+  let ref = useRef()
+  let { buttonProps } = useButton(props, ref)
+  let { children } = props
+
+  console.log(buttonProps)
+
+  return <button className="bg-calico-orange-300" {...props} {...buttonProps} />
 }
