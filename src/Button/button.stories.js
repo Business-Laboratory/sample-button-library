90011ca4b6e4161f0d02f6cda953fc8421027561
Wesley White <Wesley.white35@gmail.com>
2020-08-04 15:27:22 -0500

    added button story with knobs for color and disabled status

--- src/Button/button.stories.js
+++ src/Button/button.stories.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Main, Button } from '..'
 import useNumberOfCols from '../useNumberOfCols'
+import { withKnobs, boolean, select } from '@storybook/addon-knobs'
 
-export default { title: 'My Button' }
+export default { title: 'My Button', decorators: [withKnobs] }
 
 export const NormalButton = () => {
   const { ref, nCols } = useNumberOfCols()
@@ -10,6 +11,33 @@ export const NormalButton = () => {
   return (
     <Main ref={ref}>
       <Button className={`${getColRange(nCols)} h-12`} color="copper">
+        Button
+      </Button>
+    </Main>
+  )
+}
+
+export const ButtonWithKnobs = () => {
+  const { ref, nCols } = useNumberOfCols()
+
+  const label = 'Colors'
+  const options = [
+    'copper',
+    'matisse-red',
+    'scout-green',
+    'lichen-green',
+    'matisse-blue',
+    'calico-orange',
+  ]
+  const defaultValue = 'copper'
+
+  return (
+    <Main ref={ref}>
+      <Button
+        className={`${getColRange(nCols)} h-12`}
+        color={select(label, options, defaultValue)}
+        disabled={boolean('Disabled', false)}
+      >
         Button
       </Button>
     </Main>
